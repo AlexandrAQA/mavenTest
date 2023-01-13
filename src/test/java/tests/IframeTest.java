0d26f50ff693c410ea685000e8c51f3e06a43b45
Alex Panaev <panaev.as@gmail.com>
2023-01-13 13:11:52 +0400

    main: maven test project Refactoring

--- src/test/java/tests/IframeTest.java
+++ src/test/java/tests/IframeTest.java
@@ -18,29 +18,4 @@ public class IframeTest extends BaseTest {
         driver.navigate().back();
 
     }
-
-    @Test
-    public void iframeTest2() {
-        driver.get("https://the-internet.herokuapp.com/iframe");
-        driver.switchTo().frame("mce_0_ifr");
-        String text = driver.findElement(By.id("tinymce")).
-                findElement(By.cssSelector("p")).getText();
-        Assert.assertEquals(text, "Your content goes here.");
-        driver.switchTo().defaultContent();
-        driver.findElement(By.cssSelector("button[title='Align center'")).click();
-//        driver.navigate().back();
-
-    }
-
-    @Test
-    public void iframeTest3() {
-        driver.get("https://the-internet.herokuapp.com/iframe");
-        driver.switchTo().frame("mce_0_ifr");
-        String text = driver.findElement(By.id("tinymce")).
-                findElement(By.cssSelector("p")).getText();
-        Assert.assertEquals(text, "Your content goes here.");
-        driver.switchTo().defaultContent();
-        driver.findElement(By.cssSelector("button[title='Align center'")).click();
-        //driver.navigate().back();
-    }
 }
