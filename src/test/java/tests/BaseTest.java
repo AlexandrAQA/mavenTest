36cab28bc3340e1f0f72486dc732349f818b6921
Alex Panaev <panaev.as@gmail.com>
2023-01-14 15:40:27 +0400

    main: maven: Refactoring, added Edge+regression +

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -3,18 +3,30 @@ package tests;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 
 import java.time.Duration;
 
 public class BaseTest {
+
     WebDriver driver;
 
+    @Parameters("browser")
     @BeforeMethod
-    public void setUp() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+    public void setUp(String browser) {
+        if (browser.equals("chrome")){
+            WebDriverManager.chromedriver().setup();
+            ChromeOptions options = new ChromeOptions();
+            options.setHeadless(true);//we can't see how browser executes test
+            driver = new ChromeDriver();
+        } else if(browser.equals("edge")){
+            WebDriverManager.edgedriver().setup();
+            driver=new EdgeDriver();
+        }
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.get("https://the-internet.herokuapp.com/");
