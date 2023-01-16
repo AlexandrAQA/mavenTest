4c6881e2f05c28851faa2b1c9f3b275ec79711c3
Alex Panaev <panaev.as@gmail.com>
2023-01-16 17:52:57 +0400

    main:maven:Assert_Fail

--- src/test/java/tests/InputsTest.java
+++ src/test/java/tests/InputsTest.java
@@ -3,11 +3,21 @@ package tests;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class InputsTest extends BaseTest{
     @Test
     public void TestInputs(){
+        driver.findElement(By.linkText("Inputs")).click();
+        WebElement input = driver.findElement(By.tagName("input"));
+        input.sendKeys(Keys.ARROW_UP);
+
+        driver.navigate().back();
+    }
+    @Test
+    public void TestInputsFail(){
+        Assert.fail(); //to fail test
         driver.findElement(By.linkText("Inputs")).click();
         WebElement input = driver.findElement(By.tagName("input"));
         input.sendKeys(Keys.ARROW_UP);
