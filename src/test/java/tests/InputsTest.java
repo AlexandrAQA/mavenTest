dbe2fafb6a0dbac7c716b8d5bb508ccfadc3210a
Alex Panaev <panaev.as@gmail.com>
2023-01-16 21:41:33 +0400

    @Epic/feature/story/description

--- src/test/java/tests/InputsTest.java
+++ src/test/java/tests/InputsTest.java
@@ -1,16 +1,19 @@
 package tests;
 
-import io.qameta.allure.Issue;
-import io.qameta.allure.Link;
-import io.qameta.allure.TmsLink;
+import io.qameta.allure.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+@Epic("Big Epic")
+@Feature("New Feature")
 public class InputsTest extends BaseTest{
+
+    @Story("Inputs story")
     @Test
+    @Description("Inputs validation")
     public void TestInputs(){
         driver.findElement(By.linkText("Inputs")).click();
         WebElement input = driver.findElement(By.tagName("input"));
@@ -18,6 +21,7 @@ public class InputsTest extends BaseTest{
 
         driver.navigate().back();
     }
+
     @Issue("Bug-01")
     @TmsLink("case=79")
     @Link("https://github.com/AlexandrAQA") //url on docs or important info
