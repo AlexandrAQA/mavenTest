af8238ae7addce745134395d84222fb5e3f15044
Alex Panaev <panaev.as@gmail.com>
2023-01-18 23:27:02 +0400

    added takeScreenshot method + TestListener Refactoring

--- src/test/java/utils/TestListener.java
+++ src/test/java/utils/TestListener.java
@@ -1,5 +1,8 @@
 package utils;
 
+import org.openqa.selenium.NoSuchSessionException;
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
@@ -7,28 +10,42 @@ import java.util.concurrent.TimeUnit;
 
 public class TestListener implements ITestListener {
 
-    @Override
-    public void onTestStart(ITestResult iTestResult){
-        System.out.println(String.format("================= TEST IS RUNNING %s ================", iTestResult));
+    public void onTestStart(ITestResult iTestResult) {
+        System.out.println((String.format("======================================== STARTING TEST %s ========================================", iTestResult.getName())));
     }
 
-    @Override
-    public void onTestSuccess(ITestResult iTestResult){
-        System.out.println(String.format("================= FINISHED TEST %s Duration: %ss ================", iTestResult,
-                getExecutionTime(iTestResult)));
-    }
-    @Override
-    public void onTestFailure(ITestResult iTestResult){
-        System.out.println(String.format("=================!!! FAILED TEST %s Duration: %ss ================", iTestResult,
+    public void onTestSuccess(ITestResult iTestResult) {
+        System.out.println(String.format("======================================== FINISHED TEST %s Duration: %ss ========================================", iTestResult.getName(),
                 getExecutionTime(iTestResult)));
     }
 
-    @Override
-    public void onTestSkipped(ITestResult iTestResult){
-        System.out.println(String.format("================= SKIPPED TEST %s ================", iTestResult));
+    public void onTestFailure(ITestResult iTestResult) {
+        System.out.println(String.format("======================================== FAILED TEST %s Duration: %ss ========================================", iTestResult.getName(),
+                getExecutionTime(iTestResult)));
+        takeScreenshot(iTestResult);
     }
 
-    private long getExecutionTime(ITestResult iTestResult){
-        return TimeUnit.MILLISECONDS.toSeconds(iTestResult.getEndMillis()-iTestResult.getStartMillis());
+    public void onTestSkipped(ITestResult iTestResult) {
+        System.out.println(String.format("======================================== SKIPPING TEST %s ========================================", iTestResult.getName()));
+        takeScreenshot(iTestResult);
+    }
+
+    private byte[] takeScreenshot(ITestResult iTestResult) {
+        ITestContext context = iTestResult.getTestContext();
+        try {
+            WebDriver driver = (WebDriver) context.getAttribute("driver");
+            if(driver != null) {
+                return AllureUtils.takeScreenshot(driver);
+            } else {
+                return new byte[] {};
+            }
+        } catch (NoSuchSessionException | IllegalStateException ex) {
+            return new byte[] {};
+        }
+    }
+
+
+    private long getExecutionTime(ITestResult iTestResult) {
+        return TimeUnit.MILLISECONDS.toSeconds(iTestResult.getEndMillis() - iTestResult.getStartMillis());
     }
 }
